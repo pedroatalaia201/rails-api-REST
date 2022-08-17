bb7f1068557c29b56524350f8e25e2e60e5e037b
Pedro <pedrocondatalaia@hotmail.com>
2022-08-17 15:35:00 -0300

    feat: update_only: true for address

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,8 @@ class Contact < ApplicationRecord
     has_one :address
 
     accepts_nested_attributes_for :phones, allow_destroy: true
-    accepts_nested_attributes_for :address
+    accepts_nested_attributes_for :address, update_only: true 
+    # Only update the Address for  Contact, do not create another record.
 
     def as_json(options = {})
         super(
